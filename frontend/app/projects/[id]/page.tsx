b05853bde5bff19438f41df3a94df9ecdc9d235a
taladabbagh <dabbagh.tala@gmail.com>
2025-07-07 13:41:44 +0300

    CON-61-Re-added project media to project description

--- frontend/app/projects/[id]/page.tsx
+++ frontend/app/projects/[id]/page.tsx
@@ -4,7 +4,6 @@ import { ProjectHeader } from "@/components/projects/nav/details/ProjectHeader"
 import { ProjectDescription } from "@/components/projects/nav/details/ProjectDescription"
 import { ProjectMedia } from "@/components/projects/nav/details/ProjectMedia"
 import { ProjectNextSteps } from "@/components/projects/nav/details/ProjectNextSteps"
-import { ProjectReviews } from "@/components/projects/nav/details/ProjectReviews"
 import { projectService } from "@/app/services/projects/project-service"
 
 interface DetailsPageProps {
@@ -24,18 +23,13 @@ export default async function DetailsPage(props: DetailsPageProps) {
   return (
     <div className="space-y-6">
       <div className="flex flex-col space-y-6">
-        {/* <ProjectBanner bannerUrl={project.banner_url} /> */}
         <ProjectHeader project={project} />
       </div>
 
       <ProjectNav projectId={params.id} />
       <ProjectDescription description={project.description} />
-      <ProjectMedia />
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         <ProjectNextSteps />
-        <ProjectReviews />
-      </div>
     </div>
   )
 }
